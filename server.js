59316d886266c296155f51475fcda3bed247fd28
jordan <jordan@jordans-MacBook-Air.local>
2020-01-20 22:15:22 +0700

    almost mvp

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+//const db = require('./data/dbConfig.js');
+let accountrouter = require('./account/accountrouter');
+
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountrouter)
 
 module.exports = server;
